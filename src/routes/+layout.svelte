4739c1e1ea522cdb6405a27685a200ec693cde10
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-03-03 16:16:26 +0530

    make it fune

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import "../app.css";
   import { createThemeSwitcher, Theme } from 'svelte-theme-select';
-  import Icon from 'svelte-awesome';
   import { github, linkedin } from 'svelte-awesome/icons';
+  import { Icon } from 'svelte-awesome';
 
   let store = createThemeSwitcher()
   let val = 'system'
@@ -25,17 +25,16 @@
 
   <div class="flex flex-row gap-5 items-center">
     <p class="p-2 text-4xl">Indian Constitution</p>
-    <a href="https://github.com/BoogieMonster1O1/icystudy">
-      <Icon data={github} />
+    <a href="https://github.com/BoogieMonster1O1/icystudy" target="_blank">
+      <Icon data={github} scale={2} />
     </a>
-    <a href="https://www.linkedin.com/in/shrish-deshpande-6287561a9">
-      <Icon data={linkedin} />
+    <a href="https://www.linkedin.com/in/shrish-deshpande-6287561a9" target="_blank">
+      <Icon data={linkedin} scale={2} />
     </a>
-    <p class="text-sm">© GNU AGPL</p>
   </div>
   <div class="flex flex-row flex-none items-center">
     <p class="p-2">Theme: </p>
-    <select bind:value={val} on:change={() => {setTheme(val)}} class="dark:bg-neutral-700 appearance-none w-24 flex-none text-lg m-4 px-1 ml-0">
+    <select bind:value={val} on:change={() => {setTheme(val)}} class="dark:bg-neutral-700 appearance-none w-24 flex-none text-lg m-4 px-1 ml-0 text-center">
       <option value="system">System</option>
       <option value="light">Light</option>
       <option value="dark">Dark</option>
@@ -44,5 +43,9 @@
 </nav>
 
 <slot />
+
+<footer class="flex flex-row justify-center">
+  <p class="text-sm">© GNU AGPL</p>
+</footer>
 
 <Theme />
